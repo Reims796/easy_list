854c37ce9e0c2192f1e3db6a913cf651844cfa6c
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-11-05 18:54:35 +0300

    Update ft_rev_par_list.py

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -1,25 +1,14 @@
-def ft_len(str):
+def len_mass(mass):
     a = 0
-    for i in str:
+    for i in mass:
         a += 1
     return a
 
 
 def ft_rev_par_list(mass):
-    i = 1
-    x = []
-    d = ft_len(mass)
-    if d % 2 == 0:
-        for i in range(d):
-            x.append(mass[i])
-            x.append(mass[i-1])
-            i += 2
-        return x
-    else:
-        d = d - 1
-        for i in range(d):
-            x.append(mass[i])
-            x.append(mass[i-1])
-            i += 2
-        x.append(mass[d])
-        return x
+    for i in range(1, len_mass(mass), 2):
+        b = mass[i - 1]
+        c = mass[i]
+        mass[i - 1] = c
+        mass[i] = b
+    return mass
