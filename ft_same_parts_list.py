f9683e463c11b5651605b2ad0e1bf5041c344049
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-11-05 11:16:05 +0300

    Update ft_same_parts_list.py

--- ft_same_parts_list.py
+++ ft_same_parts_list.py
@@ -1,20 +1,7 @@
-def ft_len_mass(mass):
-    a = 0
-    for i in mass:
-        a += 1
-    return a
-
-
 def ft_same_parts_list(mass):
-    a = list()
-    i = 1
-    x = 0
-    for i in range(0, ft_len(mass)):
-        if (mass[i] > 0 and mass[i-1] > 0) or (mass[i] < 0 and mass[i-1] < 0):
-            x += 1
-        if mass[i] == 0 and mass[i-1] == 0:
-            x += 1
-    if x > 0:
-        return True
-    if x == 0:
-        return False
+    f = len(mass)
+    x = False
+    for i in range(1, f):
+        if mass[i - 1] < 0 and mass[i] < 0 or mass[i - 1] > 0 and mass[i] > 0:
+            x = True
+    return x
